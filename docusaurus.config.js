0699972785ec6c616fae1a081fa64141d2d4d6d0
Better Me <betterme@Jinjuui-MacBookPro.local>
2025-10-24 11:33:00 +0900

    config url 최종 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/gotjinju',
+  url: 'https://gotjinju.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/gembox/',
